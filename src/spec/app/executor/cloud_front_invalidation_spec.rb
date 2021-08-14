bb694e72f87cd72e023fcf49212ef1c593bd4fcb
ljourm <ljourm@gmail.com>
2021-08-14 19:25:12 +0900

    update

---
+++ src/spec/app/executor/cloud_front_invalidation_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe App::Executor::CloudFrontInvalidation do
+  let(:executor) { described_class.new(event: event, context: context) }
+  let(:event) { {} }
+  let(:context) { {} }
+
+  it '#execute' do
+    expect { executor }.not_to raise_exception
+  end
+end
